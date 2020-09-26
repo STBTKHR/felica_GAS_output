df3b7f0014dfd086db2374635ae217883c48ba52
iemoto <reyryuuu@gmail.com>
2020-09-27 00:51:39 +0900

    out

---
+++ felica_test/felica_output.py
@@ -0,0 +1,12 @@
+import nfc
+import binascii
+
+def conncted(tag):
+    idm = binascii.hexlify(tag.idm)
+
+    print(idm   )
+    return False
+
+clf = nfc.ContactlessFrontend('usb')
+tag = clf.connect(rdwr = {'on-connect': conncted})
+clf.close()
--- felica_test/felica_read.py
+++
@@ -1,5 +0,0 @@
-import requests
-
-url = 'https://script.google.com/macros/s/AKfycbxDF8RfY1wELzum7kjy-W1qBL98jAxen59CGQk92-bw_YPka9E/exec$data1=hoge'
-
-requests.get(url)
